dedda78a6e1887a5d262b3f034bbe2a91f7bb7d5
lmj0927 <lmjj0927@naver.com>
2025-02-03 13:32:14 +0900

    [Feat] ProgressUI 구현

---
+++ Assets/Scripts/UI/ProgressBar.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressBar : MonoBehaviour
+{
+    public Image image;
+    private Slider _slider;
+    private Camera _mainCamera;
+    private Camera _uiCamera;
+    private Canvas _canvas;
+    private Transform _owner;
+    
+    void Awake()
+    {
+        _slider = GetComponent<Slider>();
+        ResetBar();
+        _canvas = GetComponentInParent<Canvas>();
+        _mainCamera = Camera.main;
+        _uiCamera = _canvas.worldCamera;
+    }
+    
+    public void SetBar(float value)
+    {
+        _slider.maxValue = value;
+    }
+
+    public void ResetBar()
+    {
+        _slider.value = 0;
+    }
+
+    public void UpdateProgressBar(float value)
+    {
+        _slider.value = value;
+    }
+
+    public void SetColor(Color color)
+    {
+        image.color = color;
+    }
+}
