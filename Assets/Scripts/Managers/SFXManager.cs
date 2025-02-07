665a24401944469feab645540548b9b00dafa7c3
oriorori <junha3679@gmail.com>
2025-02-07 15:49:26 +0900

    [Feat] SoundManager 구현 및 샘플 SFXData 추가
    작동 및 사용 방식은 VFXManager와 동일

---
+++ Assets/Scripts/Managers/SFXManager.cs
@@ -0,0 +1,140 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
+using Managers;
+using UnityEngine;
+
+public class SFXManager : SingletonDontDestroy<SFXManager>
+{
+    Dictionary<SFXType, Queue<AudioSource>> sfxPools = new Dictionary<SFXType, Queue<AudioSource>>();
+    Dictionary<SFXType, SFXDataSO> sfxDataSOs = new Dictionary<SFXType, SFXDataSO>();
+
+    private async void Start()
+    {
+        await Initialize();
+    }
+    
+    public async UniTask Initialize()
+    {
+        await LoadSFXSources("Loader");
+    }
+
+    async UniTask LoadSFXSources(string sceneName)
+    {
+        // SceneName에서 필요한 sfx들을 pooling 해놓는다
+        // 해당 Scene에서 필요한 sfx의 종류는 SFXLoadSO scriptableObject에서 관리한다.
+        SFXLoadSO sfxLoadSO = null;
+        switch (sceneName)
+        {
+            case "Loader":
+                sfxLoadSO = await DataManager.Instance.LoadDataAsync<SFXLoadSO>(Addresses.Data.FX.SFXLOADER);
+                break;
+            default:
+                Debug.LogError($"{sceneName} not found!");
+                break;
+        }
+        foreach (var sfxDataSO in sfxLoadSO.sfxDataSOs)
+        {
+            Instance.RegisterSFX(sfxDataSO);
+        }
+    }
+    
+    private void RegisterSFX(SFXDataSO sfxDataSO)
+    {
+        if (!sfxPools.ContainsKey(sfxDataSO.sfxType))
+        {
+            sfxPools[sfxDataSO.sfxType] = new Queue<AudioSource>();
+            sfxDataSOs[sfxDataSO.sfxType] = sfxDataSO;
+        }
+    }
+
+    public AudioSource PlaySFX(SFXType sfxType, Vector3 position)
+    {
+        if (!sfxPools.ContainsKey(sfxType))
+        {
+            Debug.LogError($"{sfxType} doesn't exist in SFXPools!");
+            return null;
+        }
+
+        AudioSource source = null;
+        if (sfxPools[sfxType].Count <= 0)
+        {
+            source = CreateAudioSource(sfxDataSOs[sfxType]);
+        }
+        else
+        {
+            source = sfxPools[sfxType].Dequeue();
+        }
+
+        source.transform.position = position;
+        source.Play();
+
+        if (!source.loop)
+        {
+            ReturnSFX(sfxType, source, source.clip.length);
+        }
+
+        return source;
+    }
+    
+    public AudioSource PlaySFX(SFXType sfxType, Transform parent)
+    {
+        if (!sfxPools.ContainsKey(sfxType))
+        {
+            Debug.LogError($"{sfxType} doesn't exist in SFXPools!");
+            return null;
+        }
+
+        AudioSource source = null;
+        if (sfxPools[sfxType].Count <= 0)
+        {
+            source = CreateAudioSource(sfxDataSOs[sfxType]);
+        }
+        else
+        {
+            source = sfxPools[sfxType].Dequeue();
+        }
+        
+        source.transform.position = parent.position;
+        source.transform.SetParent(parent);
+        source.Play();
+
+        if (!source.loop)
+        {
+            ReturnSFX(sfxType, source, source.clip.length);
+        }
+
+        return source;
+    }
+    
+    private AudioSource CreateAudioSource(SFXDataSO sfxData)
+    {
+        GameObject obj = new GameObject($"SFX_{sfxData.sfxType}");
+        obj.transform.SetParent(transform);
+        AudioSource audioSource = obj.AddComponent<AudioSource>();
+
+        audioSource.clip = sfxData.clip;
+        audioSource.volume = sfxData.volume;
+        audioSource.loop = sfxData.loop;
+        audioSource.playOnAwake = false;
+
+        return audioSource;
+    }
+    
+    public void ReturnSFX(SFXType sfxType, AudioSource audioSource)
+    {
+        audioSource.Stop();
+        audioSource.transform.SetParent(Instance.transform);
+        sfxPools[sfxType].Enqueue(audioSource);
+    }
+
+    private async UniTask ReturnSFX(SFXType sfxType, AudioSource audioSource, float length)
+    {
+        await UniTask.Delay(TimeSpan.FromSeconds(length));
+        audioSource.Stop();
+        audioSource.transform.SetParent(Instance.transform);
+        sfxPools[sfxType].Enqueue(audioSource);
+    }
+}
+
---
+++ Assets/Scripts/ScriptableObjects/Data/SFXLoadSO.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName= "SFXLoadSO", menuName = "SO/FX/SFXLoadSO")]
+public class SFXLoadSO : ScriptableObject
+{
+    public List<SFXDataSO> sfxDataSOs;
+}
