9f5200644ab320686614747fc4d0715eabab1d9a
Lee Minhwan <59609086+minhvvan@users.noreply.github.com>
2025-02-25 23:35:30 +0900

    [Refactor] BarrierUI 삭제
    - Address 수정
    - Prefab 삭제
    - Script 삭제

--- Assets/Scripts/Utils/Addresses.cs
+++ Assets/Scripts/Utils/Addresses.cs
@@ -112,8 +112,6 @@ public static class Addresses
         
         public static class UI
         {
-            public const string BARRIER_FLAT = "prefabs/ui/barrier_flat";
-            public const string BARRIER_CORNER = "prefabs/ui/barrier_corner";
         }
         
         public static class Enemy
